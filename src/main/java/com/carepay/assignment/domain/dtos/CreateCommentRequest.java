2bcd4bee8196ade43609f60cacc578e7730d39f4
tobioye88 <tobioye88@gmail.com>
2022-03-06 16:51:31 +0100

    Assuming comment max size = 128

--- src/main/java/com/carepay/assignment/domain/dtos/CreateCommentRequest.java
+++ src/main/java/com/carepay/assignment/domain/dtos/CreateCommentRequest.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Size;
  */
 public class CreateCommentRequest {
     @NotNull
-    @Size(max = 160)
+    @Size(max = 128)
     private String comment;
 
     public CreateCommentRequest() {
